c57a8c1f667a5af0c3884a0a7ffce0fe2427b12c
Zaur <rzb@tigrio.ru>
2018-10-02 12:26:03 +0300

    changed url

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from .views import home, model_form_upload, resize_image
 
 urlpatterns = [
     path('', home, name='home'),
-    path('upload/', model_form_upload, name='model_form_upload'),
-    path('resize/<str:fname>/', resize_image, name='resize_image'),
+    path('upload/', model_form_upload, name='upload'),
+    path('<str:fname>/', resize_image, name='resize'),
 ]
